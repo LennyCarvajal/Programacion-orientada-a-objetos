d33710f705eee6d6c891a8ec51c9136f91887446
LennyCarvajal <142321581+LennyCarvajal@users.noreply.github.com>
2023-08-25 21:26:11 -0500

    Update POOER12.java

--- Seguimiento 1/POOER12.java
+++ Seguimiento 1/POOER12.java
@@ -1,5 +1,16 @@
 // Ejercicio resuelto #12
 
+/*
+Enunciado:
+Determinar la cantidad de dinero recibida por un trabajador por concepto de las horas
+semanales trabajadas en una empresa, sabiendo que cuando las horas de trabajo exceden
+de 40, el resto se considera horas extras y se pagan al doble de una hora normal, cuando
+no exceden de 8; si las horas extras exceden de 8, se pagan las primeras 8 al doble de lo
+que se paga una hora normal y el resto al triple. Del trabajador se conocen los siguientes
+datos: nombres, número de horas trabajadas en la semana y valor recibido por una hora
+normal de trabajo.
+*/
+
 import java.util.Scanner;
 
 class POOER12 {
