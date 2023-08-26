73a95c80614829c47c330c9e5fd7eb29e8251564
LennyCarvajal <142321581+LennyCarvajal@users.noreply.github.com>
2023-08-25 21:27:09 -0500

    Update POOER14.java

--- Seguimiento 1/POOER14.java
+++ Seguimiento 1/POOER14.java
@@ -1,5 +1,20 @@
 // Ejercicio resuelto #14
 
+/*
+Enunciado:
+Una empresa con tres departamentos tiene establecido un plan de incentivos para sus
+vendedores. Al final del período, a cada departamento se le pide el importe global de las
+ventas. A los departamentos que excedan el 33% de las ventas totales se les adiciona al
+salario de los vendedores un porcentaje equivalente al 20% del salario mensual. Las
+nóminas de los tres departamentos son iguales. Si se tienen los siguientes datos:
+• Ventas del departamento 1
+• Ventas del departamento 2
+• Ventas del departamento 3
+• Salario de los vendedores de cada departamento
+Hacer un algoritmo que determine cuánto recibirán los vendedores de cada departamento
+al finalizar el período.
+*/
+
 import java.util.Scanner;
 
 public class POOER14 {
